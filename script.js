69820a608b8bdda258855945a78225851404b4d4
kskannandotwin <kskannan.win@gmail.com>
2024-01-20 17:32:00 +0530

    20.01.2024-pvo
    completed

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-var personProto = {
-  calculateAge: function () {
-    console.log(2016 - this.yearOfBirth);
-  },
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+  name: "John",
+  age: 26,
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+  name: "Jonas",
+  city: "Lisbon",
 };
 
-var john = Object.create(personProto);
-john.name = "John";
-john.yearOfBirth = 1990;
-john.job = "teacher";
+function change(a, b) {
+  a = 30;
+  b.city = "San Francisco";
+}
 
-var jane = Object.create(personProto, {
-  name: { value: "Jane" },
-  yearOfBirth: { value: 1969 },
-  job: { value: "designer" },
-});
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
